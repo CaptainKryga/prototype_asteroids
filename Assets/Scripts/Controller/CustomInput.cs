cac08a5a06a8bb186461b2f566eec7a978fefd4c
CaptainKryga <krygadenis@gmail.com>
2022-10-19 14:04:06 +0200

    upd custom input logic

--- Assets/Scripts/Controller/CustomInput.cs
+++ Assets/Scripts/Controller/CustomInput.cs
@@ -1,9 +1,20 @@
+using System;
 using UnityEngine;
 
 namespace Controller
 {
     public class CustomInput : MonoBehaviour
     {
-    
+        public Action<Vector2> UpdateWASD_Action;
+        public Action<Vector2> UpdateMouseClick_Action;
+
+        private void Update()
+        {
+            Vector2 wasd = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            UpdateWASD_Action?.Invoke(wasd);
+
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+                UpdateMouseClick_Action?.Invoke(Input.mousePosition);
+        }
     }
 }
