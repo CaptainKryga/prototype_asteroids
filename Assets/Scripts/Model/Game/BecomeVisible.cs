7051c3bd110e69919471c91e383d18b106bd335f
CaptainKryga <krygadenis@gmail.com>
2022-10-19 15:47:31 +0200

    add become invisible and visible destroy go

---
+++ Assets/Scripts/Model/Game/BecomeVisible.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Model.Game
+{
+    public class BecomeVisible : MonoBehaviour
+    {
+        private bool _isVisible;
+        void OnBecameInvisible()
+        {
+            if (_isVisible)
+                Destroy(gameObject);
+        }
+
+        private void OnBecameVisible()
+        {
+            _isVisible = true;
+        }
+    }
+}
