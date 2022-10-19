213337ebe148240c6b481ca0c19cc9e1afb43f1b
CaptainKryga <krygadenis@gmail.com>
2022-10-19 14:05:04 +0200

    upd entity and player logic

---
+++ Assets/Scripts/Model/Entity.cs
@@ -0,0 +1,31 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Model
+{
+    public abstract class Entity : MonoBehaviour
+    {
+        [SerializeField] protected Rigidbody2D Rb;
+        [SerializeField] protected Transform Respawn;
+        
+        private int _lifeCount;
+        protected int LifeCount
+        {
+            get => _lifeCount;
+            set
+            {
+                _lifeCount = value;
+                if (_lifeCount <= 0)
+                {
+                    Death();
+                }
+            }
+        }
+
+        public abstract void Init(Vector3 velocity);
+        
+        protected abstract void OnCollisionEnter2D(Collision2D other);
+
+        protected abstract void Death();
+    }
+}
---
+++ Assets/Scripts/Model/Ship.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Controller;
+using UnityEngine;
+
+namespace Model
+{
+    public class Ship : Entity
+    {
+        [SerializeField] private CustomInput _customInput;
+        
+        public Action<int> UpdateState_Action;
+
+        public override void Init(Vector3 velocity)
+        {
+            transform.position = Respawn.position;
+            Rb.velocity = velocity;
+
+            LifeCount = 3;
+            
+            _customInput.UpdateWASD_Action += Move;
+            _customInput.UpdateMouseClick_Action += Attack;
+            
+            Debug.Log("Init");
+        }
+
+        protected override void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.GetComponent<Entity>())
+            {
+                LifeCount--;
+                UpdateState_Action?.Invoke(LifeCount);
+            }
+        }
+
+        protected override void Death()
+        {
+            _customInput.UpdateWASD_Action -= Move;
+            _customInput.UpdateMouseClick_Action -= Attack;
+            Debug.Log("Death");
+        }
+
+        private void Move(Vector2 vec)
+        {
+            if ((Rb.velocity + vec).magnitude > 10)
+            {
+                Rb.velocity += vec * Time.deltaTime;
+            }
+            else
+            {
+                Rb.velocity += vec;
+            }
+
+            Rb.velocity *= .9f;
+        }
+
+        private void Attack(Vector2 mouse)
+        {
+            
+        }
+    }
+}
